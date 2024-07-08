bc41e6c95a2e93d27c4abf641094bae0046a8e9f
CodeDoses <mayaethomp@gmail.com>
2024-07-07 20:28:49 -0700

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -1,7 +1,7 @@
 // Maya Thompson
 // CS 1, Section# 0107
 // Assignment #4, Problem #1
-// Summary: This program find
+// Summary: This program finds all Perfect Numbers that fall between two integer values inputted by the user
 
 #include <iostream>
 
@@ -9,7 +9,16 @@ using namespace std;
 
 int main() {
 
+// Declare and initialize variables
+int startval = 0;
+int endval = 0;
+int outerval = 0;
+void isAFactor(int, int);
+bool factor = true;
 
+
+// Prompt the user to input two integer values
+  cout << 
 
 
 
