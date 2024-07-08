1454c7855d019159d00b1088787fc92d78b8efcf
Maya Thompson <mayaethomp@gmail.com>
2024-07-08 00:54:56 -0700

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -12,15 +12,31 @@ int main() {
 // Declare and initialize variables
 int startval = 0;
 int endval = 0;
-int outerval = 0;
 void isAFactor(int, int);
 bool factor = true;
 
+int perfect;
 
 // Prompt the user to input two integer values
-  cout << 
+  cout << "Enter starting integer: " << endl;
+  cin >> startval;
 
+  cout << "Enter ending integer: " << endl;
+  cin >> endval; 
 
+// Create a loop that steps through the range of numbers between startval and endval
+  for (int outerval = startval; outerval <= endval; outerval++) {
+    // Create an inner loop that will step through the values from 1 to (outerval/2) to determine the factors of the number
+      
+  }
+
+// Output perfect number to screen
+
+  if () {
+    cout << perfect << " is a perfect number." << endl;
+  } else {
+    cout << "There is no perfect number between " << startval << " and " << endval << endl;
+  }
 
   return 0;
 
