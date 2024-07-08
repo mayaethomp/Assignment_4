12191233f1e7199dc42b59efff704995dca14264
Maya Thompson <mayaethomp@gmail.com>
2024-07-08 01:08:44 -0700

    new file problem2

---
+++ problem2.cpp
@@ -0,0 +1,5 @@
+// Maya Thompson
+// CS 1, Section# 0107
+// Assignment #4, Problem #2
+// Summary: This program creates a rock, paper, scissors game between the user and a computer (randomized picks)
+
