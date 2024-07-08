01c40cc92d72aca4526c114428a2fbb91914ad04
CodeDoses <mayaethomp@gmail.com>
2024-07-08 12:48:23 -0700

    rock paper scissors -- randomized number library included

--- problem2.cpp
+++ problem2.cpp
@@ -3,3 +3,44 @@
 // Assignment #4, Problem #2
 // Summary: This program creates a rock, paper, scissors game between the user and a computer (randomized picks)
 
+#include <iostream>
+// Library that allows randomized number
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+
+// Declare and initialize variables
+int user = 0;
+int computer = 0;
+
+
+
+// Declare functions
+char userChoice();
+char computerChoice();
+void determineWinner(char, char);
+bool playAgain();
+
+
+// Generate a random number for the computer's choice
+srand(time(NULL));
+int computer = rand() % 3 + 1;
+
+
+
+// Prompt the user to choose "rock," "paper," or "scissors" using a menu in a function
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
