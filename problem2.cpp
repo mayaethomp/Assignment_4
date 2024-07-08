f33bba1bfad5623fc581e82652dc486b85748a87
CodeDoses <mayaethomp@gmail.com>
2024-07-08 13:49:43 -0700

    check if computer choice results to error?

--- problem2.cpp
+++ problem2.cpp
@@ -26,50 +26,57 @@ int main() {
 // Convert randomize number to char computer's choice
   char computerChoice() {
     // int(computer); ??
-  };
+    if (computer == 1) {
+      computer == 'R';
+    } else if (computer == 2) {
+      computer == 'P';
+    } else if (computer == 3) {
+      computer == 'S';
+    }
+  }
 
 // Prompt the user to choose "rock," "paper," or "scissors" using a menu in a function
-char userChoice() {
-  cout << "Welcome Rock / Paper / Scissors game! Here are your choices: " << endl << endl
-       << setw(14) << "R/r:  ROCK" << endl << "P/p:  PAPER" << endl << "S/s:  SCISSORS" << endl << endl;
-       cout << "Please make a selection" << endl;
-  cin >> user;
-}
+  char userChoice() {
+    cout << "Welcome Rock / Paper / Scissors game! Here are your choices: " << endl << endl
+        << setw(14) << "R/r:  ROCK" << endl << "P/p:  PAPER" << endl << "S/s:  SCISSORS" << endl << endl;
+        cout << "Please make a selection" << endl;
+    cin >> user;
+  }
 
 
 
 // Output the results of the game to the user
-void determineWinner(char, char) {
-  if (user == computer) {
-    cout << "Both you and the computer chose " << user << ". You end in a draw!" << endl;
-  } else if ((user == 'R' || user == 'r') && (computer == 'P' || computer == 'p')) {
-    cout << "You chose ROCK and the computer chose PAPER. PAPER covers ROCK. You lose. Sorry." << endl;
-  } else if ((user == 'R' || user == 'r') && (computer == 'S' || computer == 's')) {
-    cout << "You chose ROCK and the computer chose SCISSORS. ROCK crushes SCISSORS. You win!" << endl;
-  } else if ((user == 'P' || user == 'p') && (computer == 'R' || computer == 'r')) {
-    cout << "You chose PAPER and the computer chose ROCK. PAPER covers ROCK. You win!" << endl;
-  } else if ((user == 'P' || 'p') && (computer == 'S' || computer == 's')) {
-    cout << "You chose PAPER and the computer chose SCISSORS. SCISSORS cuts PAPER. You lose. Sorry." << endl;
-  } else if ((user == 'S' || user == 's') && (computer == 'R' || computer == 'r')) {
-    cout << "You chose SCISSORS and the computer chose ROCK. ROCK crushes SCISSORS. You lose. Sorry." << endl;
-  } else if ((user == 'S' || user == 's') && (computer == 'P' || computer == 'p')) {
-    cout << "You chose SCISSORS and the computer chose PAPER. SCISSORS cuts PAPER. You win!" << endl;
-  } else {
-    cout << "Invalid response entered." << endl;
+  void determineWinner(char, char) {
+    if (user == computer) {
+      cout << "Both you and the computer chose " << user << ". You end in a draw!" << endl;
+    } else if ((user == 'R' || user == 'r') && (computer == 'P') {
+      cout << "You chose ROCK and the computer chose PAPER. PAPER covers ROCK. You lose. Sorry." << endl;
+    } else if ((user == 'R' || user == 'r') && (computer == 'S') {
+      cout << "You chose ROCK and the computer chose SCISSORS. ROCK crushes SCISSORS. You win!" << endl;
+    } else if ((user == 'P' || user == 'p') && (computer == 'R') {
+      cout << "You chose PAPER and the computer chose ROCK. PAPER covers ROCK. You win!" << endl;
+    } else if ((user == 'P' || 'p') && (computer == 'S') {
+      cout << "You chose PAPER and the computer chose SCISSORS. SCISSORS cuts PAPER. You lose. Sorry." << endl;
+    } else if ((user == 'S' || user == 's') && (computer == 'R') {
+      cout << "You chose SCISSORS and the computer chose ROCK. ROCK crushes SCISSORS. You lose. Sorry." << endl;
+    } else if ((user == 'S' || user == 's') && (computer == 'P') {
+      cout << "You chose SCISSORS and the computer chose PAPER. SCISSORS cuts PAPER. You win!" << endl;
+    } else {
+      cout << "Invalid response entered." << endl;
+    }
   }
-}
 
 // Prompt the user to choose whether or not to play again and run the result through playAgain function
-bool playAgain() {
-  cout << "Would you like to play again? (Y/y for yes, N/n for No)" << endl;
-  cin >> user;
-    if (user == Y || user == y) {
-      return true;
-    } else if (user == N || user == n) {
-      return false;
-      cout << "Thank you for playing Rock / Paper / Scissors! Have a great day!";
-    }
-}
+  bool playAgain() {
+    cout << "Would you like to play again? (Y/y for yes, N/n for No)" << endl;
+    cin >> user;
+      if (user == Y || user == y) {
+        return true;
+      } else if (user == N || user == n) {
+        return false;
+        cout << "Thank you for playing Rock / Paper / Scissors! Have a great day!";
+      }
+  }
 
 
 
