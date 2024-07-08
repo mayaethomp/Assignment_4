903310cfdf759eee3c39d7d7b1341cf9728cb626
Maya Thompson <mayaethomp@gmail.com>
2024-07-08 01:06:46 -0700

    adding functions

--- problem1.cpp
+++ problem1.cpp
@@ -12,10 +12,14 @@ int main() {
 // Declare and initialize variables
 int startval = 0;
 int endval = 0;
-void isAFactor(int, int);
-bool factor = true;
 
-int perfect;
+// Use a function to determine if a number is a factor of another number
+bool isAFactor(int x, int y) {
+  return x % y == 0;
+}
+
+// Use a function to determine if a number is a perfect number
+bool isPerfect(int);
 
 // Prompt the user to input two integer values
   cout << "Enter starting integer: " << endl;
@@ -23,6 +27,8 @@ int perfect;
 
   cout << "Enter ending integer: " << endl;
   cin >> endval; 
+
+ 
 
 // Create a loop that steps through the range of numbers between startval and endval
   for (int outerval = startval; outerval <= endval; outerval++) {
@@ -32,8 +38,10 @@ int perfect;
 
 // Output perfect number to screen
 
-  if () {
-    cout << perfect << " is a perfect number." << endl;
+  if (isPerfect == true) {
+    cout << // insert variable here
+     << " is a perfect number." << endl;
+     
   } else {
     cout << "There is no perfect number between " << startval << " and " << endval << endl;
   }
