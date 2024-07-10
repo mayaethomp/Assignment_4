65ae3d878fcf9da42ad38ae78c5ab2490d83b3e6
Maya Thompson <mayaethomp@gmail.com>
2024-07-09 23:52:33 -0700

    changed to upper! how did I forget this

--- problem2.cpp
+++ problem2.cpp
@@ -15,6 +15,15 @@ using namespace std;
 char user = 'A';
 int randomNum = 0;
 
+// Declare and define a user choice function
+  char userChoice() {
+    cout << "Welcome Rock / Paper / Scissors game! Here are your choices: " << endl << endl
+        << setw(14) << "R/r:  ROCK" << endl << "P/p:  PAPER" << endl << "S/s:  SCISSORS" << endl << endl;
+        cout << "Please make a selection" << endl;
+    cin >> user;
+    return toupper(user);
+  }
+
 // Declare and define a computer choice function
   char computerChoice(int randomNum) {
     // Generate a random number for the computer's choice
@@ -34,33 +43,22 @@ int randomNum = 0;
   }
   }
 
-
-// Declare and define a user choice function
-  char userChoice() {
-    cout << "Welcome Rock / Paper / Scissors game! Here are your choices: " << endl << endl
-        << setw(14) << "R/r:  ROCK" << endl << "P/p:  PAPER" << endl << "S/s:  SCISSORS" << endl << endl;
-        cout << "Please make a selection" << endl;
-    cin >> user;
-    return user;
-  }
-
-
 // Declare and define a function to determine a winner
 string determineWinner(char user, char computer) {
     ostringstream result;
     if (user == computer) {
         result << "Both you and the computer chose " << user << ". You end in a draw!\n";
-    } else if ((user == 'R' || user == 'r') && (computer == 'P')) {
+    } else if ((user == 'R') && (computer == 'P')) {
         result << "You chose ROCK and the computer chose PAPER. PAPER covers ROCK. You lose.\n";
-    } else if ((user == 'R' || user == 'r') && (computer == 'S')) {
+    } else if ((user == 'R') && (computer == 'S')) {
         result << "You chose ROCK and the computer chose SCISSORS. ROCK crushes SCISSORS. You win!\n";
-    } else if ((user == 'P' || user == 'p') && (computer == 'R')) {
+    } else if ((user == 'P') && (computer == 'R')) {
         result << "You chose PAPER and the computer chose ROCK. PAPER covers ROCK. You win!\n";
-    } else if ((user == 'P' || user == 'p') && (computer == 'S')) {
+    } else if ((user == 'P') && (computer == 'S')) {
         result << "You chose PAPER and the computer chose SCISSORS. SCISSORS cuts PAPER. You lose.\n";
-    } else if ((user == 'S' || user == 's') && (computer == 'R')) {
+    } else if ((user == 'S') && (computer == 'R')) {
         result << "You chose SCISSORS and the computer chose ROCK. ROCK crushes SCISSORS. You lose.\n";
-    } else if ((user == 'S' || user == 's') && (computer == 'P')) {
+    } else if ((user == 'S') && (computer == 'P')) {
         result << "You chose SCISSORS and the computer chose PAPER. SCISSORS cuts PAPER. You win!\n";
     } else {
         result << "Invalid response entered.\n";
@@ -87,22 +85,20 @@ string determineWinner(char user, char computer) {
 
 int main() {
 
+  // MAIN GAME // 
+  
+  // Required to prevent endless playAgain loop??
+  bool keepPlaying = true;
+
+  while (keepPlaying) {
     // Use userChoice() function to prompt the user to choose "rock," "paper," or "scissors" using a menu in a function
     char user = userChoice();
-    // Use computerChoice() function to generate a random number to assign the computer's choice
+     // Use computerChoice() function to generate a random number to assign the computer's choice
     char computer = computerChoice(randomNum);
     // Output the results of the game to the user
     cout << determineWinner(user, computer) << endl;
     // Prompt the user to choose whether or not to play again and run the result through playAgain function
-    playAgain();
-
-// If the user responds yes to play again, rerun the program until the user answers no
-  while (playAgain() == true) {
-    
-    char user = userChoice();
-    cout << determineWinner(user, computer) << endl;
-    char computer = computerChoice(randomNum);
-    playAgain();
+    keepPlaying = playAgain();
   
   }
 
