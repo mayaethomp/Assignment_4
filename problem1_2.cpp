023541ff4ea50b9e08613022e9448522b4622b7d
CodeDoses <mayaethomp@gmail.com>
2024-07-12 13:30:10 -0700

    rough draft -- problem1_2

--- problem1_2.cpp
+++ problem1_2.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+// Declare and initialize variables
+int startval = 0;
+int endval = 0;
 
 // Function to determine if a number if a factor of another
 bool isAFactor(int x, int y) {
@@ -20,7 +23,41 @@ bool isPerfect (int num) {
     
     // Create a loop that steps through the range of numbers between startval and endval
   for (int outerval = startval; outerval <= endval; outerval++) {
-    if (isAFacor(startval, endval)) {
-        
+    if (isAFactor(startval, endval) == true) {
+        if (outerval * outerval != num) {
+            sum = sum + outerval + (num/outerval);
+        } else {
+            sum = sum + outerval;
+        }
+    if (sum == num && num != 1) {
+        return true;
+        cout << outerval << " is a perfect number." << endl;
+    } else {
+        return false;
     }
+        
+    } else {
+        cout << "There is no perfect number between " << startval << " and " << endval << endl;
+    }
+}
+}
+
+int main() {
+
+// Prompt the user to input two integer values
+  cout << "Enter starting integer: " << endl;
+  cin >> startval;
+
+  cout << "Enter ending integer: " << endl;
+  cin >> endval; 
+   
+    isAFactor(startval, endval);
+
+
+
+
+
+
+    return 0;
+
 }
